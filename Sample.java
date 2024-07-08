af91b05fc88b9961521f43ae7bc361bdfa65dfff
shubham7958 <136683471+shubham7958@users.noreply.github.com>
2024-07-08 18:52:35 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,6 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Java sample code is run from Jenkins");
+        System.out.println("Added second line to check Poll SCM option - one last time!!.");
     }
 }
